c9506464a488fd4f773fdfd78bb7d2c44d62d600
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-10-31 08:54:15 +0330

    test: add more test case

--- reverse_test.go
+++ reverse_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
 	"github.com/ServiceWeaver/weaver/weavertest"
@@ -19,7 +20,6 @@ func TestReverse(t *testing.T) {
 	for _, runner := range weavertest.AllRunners() {
 		fake := &fakeAdd{}
 		runner.Fakes = append(runner.Fakes, weavertest.Fake[PortAdd](fake))
-
 		runner.Test(t, func(t *testing.T, reverse PortReverse) {
 			ctx := context.Background()
 			got, err := reverse.Reverse(ctx, "12")
@@ -27,6 +27,22 @@ func TestReverse(t *testing.T) {
 				t.Fatal(err)
 			}
 			if want := "021"; got != want {
+				t.Fatalf("got %q, want %q", got, want)
+			}
+		})
+	}
+}
+
+func TestReverse2(t *testing.T) {
+	for _, runner := range weavertest.AllRunners() {
+		runner.Test(t, func(t *testing.T, reverse PortReverse, add PortAdd) {
+			ctx := context.Background()
+			got, err := reverse.Reverse(ctx, "12")
+			if err != nil {
+				t.Fatal(err)
+			}
+			a, _ := add.Add(ctx, 1, 2)
+			if want := strconv.Itoa(a) + "21"; got != want {
 				t.Fatalf("got %q, want %q", got, want)
 			}
 		})
